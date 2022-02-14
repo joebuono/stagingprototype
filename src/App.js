291c94e61bf894cc14480762b18bb88b99c18b18
Joe Buono <joebuono@amazon.com>
2022-02-14 16:15:42 -0500

    testing deployment to different environments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import '@aws-amplify/ui-react/styles.css';
 
 function App() {
   return (
-    <Button backgroundColor="red">BUTTON</Button>
+    <Button backgroundColor="green">BUTTON</Button>
   );
 }
 
